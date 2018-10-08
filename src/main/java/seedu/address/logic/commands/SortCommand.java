14ce0dfd3ab9e943e77ac4e6d535b7cf4066c27e
peiying98 <peiying_sg@yahoo.com.sg>
2018-10-08 22:06:51 +0800

    updating sort command

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -28,25 +28,88 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = CliSyntax.COMMAND_SORT;
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Sorts address book in ascending order"
+            + ": Sorts address book in ascending or descending order"
             + "according to the attribute specified\n"
             + "attributes includes n/, p/, e/, a/, t/, k/, r/"
             + "Parameters: "
             + "[PREFIX]\n"
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME;
+            + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + " " + REVERSE_ORDER;
 
     // might add reverse later on
 
-    public static final String MESSAGE_SORT_SUCCESS = "Sorted address book by %1$s.";
+    public static final String MESSAGE_SORT_SUCCESS = "Sorted address book by %1$s in %2$s order.";
     public static final String MESSAGE_SORT_EMPTY = "No person to sort in this attribute.";
 
     private final String attribute;
+    private final String isReverseOrder;
 
+    // Default values for sorting
+    private String sortBy = "name";
+    private String sequence = "ascending";
 
+    /**
+     * @param attribute     specify which attribute to sort by
+     * @param isReverseOrder specify if sorting is to be in ascending or descending order
+     */
+
+    public SortCommand(String attribute) {
+        requireNonNull(attribute);
+        requireNonNull(isReverseOrder);
+
+        this.field = field;
+        this.isReverseOrder = isReverseOrder;
+
+    }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        Comparator<ReadOnlyPerson> comparator = sortComparatorByPrefix(this.attribute);
+        try {
+            model.sortPersonList(comparator, isReverseOrder);
+        } catch (EmptyPersonListException eple) {
+            throw new CommandException(MESSAGE_PERSONS_LIST_EMPTY);
+        }
+
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        if (isReverseOrder) {
+            this.sequence = "descending";
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS, sortBy, sequence));
+    }
+
+    /**
+     * Comparator depending on the attribute specified
+     */
+    private Comparator<ReadOnlyPerson> sortComparatorByPrefix(String attribute) {
+        switch (attribute) {
+            case PREFIX_NAME:
+                this.sortBy = "name";
+                return (o1, o2) -> o1.getName().toString().compareToIgnoreCase(o2.getName().toString());
+            case PREFIX_PHONE:
+                this.sortBy = "phone";
+                return (o1, o2) -> o1.getPhone().toString().compareToIgnoreCase(o2.getPhone().toString());
+            case PREFIX_EMAIL:
+                this.sortBy = "email";
+                return (o1, o2) -> o1.getEmail().toString().compareToIgnoreCase(o2.getEmail().toString());
+            case PREFIX_ADDRESS:
+                this.sortBy = "address";
+                return (o1, o2) -> o1.getAddress().toString().compareToIgnoreCase(o2.getAddress().toString());
+            case PREFIX_TAG:
+                this.sortBy = "tag";
+                return (o1, o2) -> o1.getTag().toString().compareToIgnoreCase(o2.getTag().toString());
+            default:
+                return (o1, o2) -> o1.getName().toString().compareToIgnoreCase(o2.getName().toString());
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SortCommand // instanceof handles nulls
+                && field.equals(((SortCommand) other).field)
+                && REVERSE_ORDER.equals(((SortCommand) other).REVERSE_ORDER));
 
     }
 }
