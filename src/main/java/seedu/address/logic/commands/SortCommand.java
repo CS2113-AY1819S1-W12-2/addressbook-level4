aabc2244f6e32054011d52f9b6b2771da796dd45
peiying98 <peiying_sg@yahoo.com.sg>
2018-10-08 18:56:23 +0800

    sorting incomplete

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,4 +1,52 @@
+//@@author peiying98
 package seedu.address.logic.commands;
 
-public class SortCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_KPI;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_POSITION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.commons.util.FileEncryptor;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.CliSyntax;
+import seedu.address.model.Model;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+/**
+ * Commands for sorting by respective attributes
+ */
+
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = CliSyntax.COMMAND_SORT;
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts address book in ascending order"
+            + "according to the attribute specified\n"
+            + "attributes includes n/, p/, e/, a/, t/, k/, r/"
+            + "Parameters: "
+            + "[PREFIX]\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME;
+
+    // might add reverse later on
+
+    public static final String MESSAGE_SORT_SUCCESS = "Sorted address book by %1$s.";
+    public static final String MESSAGE_SORT_EMPTY = "No person to sort in this attribute.";
+
+    private final String attribute;
+
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+
+    }
 }
