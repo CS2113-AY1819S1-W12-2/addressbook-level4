8e8535cb47653174d1d9c591f2f47abf6e127bab
Limminghong <32009215+Limminghong@users.noreply.github.com>
2018-10-24 20:27:39 +0800

    Fixed error by introducing stubs

--- src/test/java/seedu/address/logic/commands/BackUpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BackUpCommandTest.java
@@ -4,30 +4,48 @@ package seedu.address.logic.commands;
 import static junit.framework.TestCase.assertEquals;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 class BackUpCommandTest {
     private static final Logger logger = Logger.getLogger(BackUpCommand.class.getName());
 
     private Model model = new ModelManager();
     private CommandHistory commandHistory = new CommandHistory();
-    private BackUpCommand backUpCommand;
     private String fileName;
+    private File tmpSource;
     private File tmpDest;
+    private String toWrite = "Hello World!";
+    private Boolean checkStub = false;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        UserPrefs userPrefs = new UserPrefs();
+        tmpSource = new File(userPrefs.getAddressBookFilePath().toString());
+        if (!tmpSource.exists()) {
+            tmpSource.createNewFile();
+            FileWriter writer = new FileWriter(tmpSource);
+            writer.write(toWrite);
+            writer.close();
+            checkStub = true;
+        }
+    }
 
     @Test
     public void execute_success() {
-        backUpCommand = new BackUpCommand();
+        BackUpCommand backUpCommand = new BackUpCommand();
         try {
-
             CommandResult result = backUpCommand.execute(model, commandHistory);
             fileName = backUpCommand.getFileName();
             assertEquals(BackUpCommand.MESSAGE_SUCCESS, result.feedbackToUser);
@@ -41,5 +59,8 @@ class BackUpCommandTest {
     public void tearDown() {
         tmpDest = new File(fileName);
         tmpDest.delete();
+        if (checkStub) {
+            tmpSource.delete();
+        }
     }
 }
